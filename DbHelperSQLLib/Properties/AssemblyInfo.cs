2414414936d689951ba54279fa454bb1e5edfdc7
Peter AuYueng <02358888@163.com>
2017-08-29 01:36:39 +0800

    property

--- DbHelperSQLLib/Properties/AssemblyInfo.cs
+++ DbHelperSQLLib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DbHelperSQLLib")]
+[assembly: AssemblyTitle("Peter DbHelperSQLLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DbHelperSQLLib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Peter DbHelperSQLLib")]
+[assembly: AssemblyCopyright("Copyright Peter ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
