564f8b6112035da708f7bd66b76dcb77a1da3f7a
Peter AuYueng <02358888@163.com>
2017-08-29 01:35:01 +0800

    this first commit

---
+++ DbHelperSQLLib/PubConstant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace DbHelperSQLLib
+{
+    public class PubConstant
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                string _connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+                //string ConStringEncrypt = ConfigurationManager.AppSettings["ConStringEncrypt"];
+                //if (ConStringEncrypt == "true")
+                //{
+                //    _connectionString = DESEncrypt.Decrypt(_connectionString);
+                //}
+                return _connectionString;
+            }
+        }
+
+        public static string GetConnectionString(string configName)
+        {
+            string connectionString = ConfigurationManager.AppSettings[configName];
+            //string ConStringEncrypt = ConfigurationManager.AppSettings["ConStringEncrypt"];
+            //if (ConStringEncrypt == "true")
+            //{
+            //    connectionString = DESEncrypt.Decrypt(connectionString);
+            //}
+            return connectionString;
+        }
+    }
+}
